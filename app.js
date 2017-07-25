6b7f299d2b967e64c7d85121dd890253ec842c7a
ricardofilipehenriquesfaria <ricardofaria@outlook.pt>
2017-07-25 17:37:57 +0100

    - Added a bounding box to search for coordinates

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ fs.readFile('./madeira.json', 'utf8', function(err, data){
 	
 	if (err) throw err;
 	
+	var lat = 32.6676611;
+	var lon = -16.9779316;
+	
+	var latFloor = Math.floor(lat *100000) / 100000;
+	var latCeil = Math.ceil(lat *100000) / 100000;
+	var lonFloor = Math.floor(lon *100000) / 100000;
+	var lonCeil = Math.ceil(lon *100000) / 100000;
+	
 	var obj = JSON.parse(data);
 	var id = 0;
 	for (var i = 0; i < obj.length; i++){
-		if(obj[i].lat == 32.6676611 && obj[i].lon == -16.9779316){
+		if(obj[i].lat >= latFloor && obj[i].lat <= latCeil && obj[i].lon >= lonFloor && obj[i].lon <= lonCeil){
 			id = obj[i].id;
 			console.log(id);
 		}	
