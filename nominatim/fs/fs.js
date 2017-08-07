b851ea02512a60322de482a56e546bf94025e05c
ricardofilipehenriquesfaria <ricardofaria@outlook.pt>
2017-08-07 23:35:20 +0100

    - Correction of some bugs

--- nominatim/fs/fs.js
+++ nominatim/fs/fs.js
@@ -8,13 +8,13 @@ module.exports = {
 			if (err) throw err;
 			
 			var obj = JSON.parse(data);
-			
 			for(var i = 0; i < obj.length; i++){
 				if(obj[i].hasOwnProperty('nodes') && obj[i].hasOwnProperty('tags') && obj[i].tags.hasOwnProperty('name')){
-					if(obj[i].tags.name != "Bus"){
+					if(obj[i].tags.name != "Bus" && obj[i].tags.name != "C"){
+						console.log(obj[i].tags.name);
 						mysql_module.getRoadsData(request_module, obj[i].tags.name);
 					}
-					wait.for.time(1);
+					wait.for.time(5);
 				}
 			}
 		})
--- nominatim/mysql/mysql.js
+++ nominatim/mysql/mysql.js
@@ -10,7 +10,15 @@ var connection = mysql.createConnection({
 
 module.exports = {
 	getRoadsData: function(request_module, road){
-		connection.query("SELECT * from roads_data WHERE road = '" + road + "'", function(err, results, fields) {
+		
+		if(road.indexOf("'") != -1){
+			road = road.replace("'", ' ');
+		}
+		
+		var query = "SELECT * from roads_data WHERE road = '" + road + "'";
+		console.log(query);
+		
+		connection.query(query, function(err, results, fields) {
 			if(results.length <= 0){
 				request_module.getBoundingBoxes(connection, road);
 			}
